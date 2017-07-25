3162b099db0f4824f23a57ca80063a2e557286f7
Jwhistlecraft <j_whistlecraft@hotmail.com>
2017-07-25 17:17:37 +0100

    commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
 
-import play.api._
+import play.api
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("hello world")
+
   }
+
+  def redirect = Action {
+    Redirect("http://www.google.com", 302)
+  }
+
+
+
+
 
 }
--- build.sbt
+++ build.sbt
@@ -11,3 +11,5 @@ scalaVersion := "2.11.11"
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
+
+
